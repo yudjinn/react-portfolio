b64ca1c82673d602e5bedda0454285a1c153a109
yudjinn <yudjinncoding@gmail.com>
2020-08-31 16:33:00 -0600

    Updates

---
+++ precache-manifest.28cfcd9f3402990ad8df8145bf4bf98b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b27ae3c85382d6de5627d00c175ba9",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "88a0cee464741f356315",
+    "url": "/react-portfolio/static/css/main.27d1164d.chunk.css"
+  },
+  {
+    "revision": "f656bbb1bcb415bc9511",
+    "url": "/react-portfolio/static/js/2.77ac6562.chunk.js"
+  },
+  {
+    "revision": "fdc84dfb581e6fb8755dd1b2b01a50e1",
+    "url": "/react-portfolio/static/js/2.77ac6562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a0cee464741f356315",
+    "url": "/react-portfolio/static/js/main.7e2f6235.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  }
+]);
